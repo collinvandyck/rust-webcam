40f95f75363b79d1f01f77c638b859a75e4d64fc
Collin Van Dyck <collin@5xx.engineer>
2025-09-03 13:09:10 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use anyhow::{Context, Result};
 use nokhwa::{
-    Camera,
-    pixel_format::RgbFormat,
-    utils::{CameraIndex, RequestedFormat, RequestedFormatType},
+    backends::capture::AVFoundationCaptureDevice, native_api_backend, pixel_format::RgbFormat, utils::{CameraIndex, RequestedFormat, RequestedFormatType}, Camera
 };
 
 fn main() -> Result<()> {
+    let be = native_api_backend().context("backend")?;
+    println!("got backend");
     let index = CameraIndex::Index(0);
+    AVFoundationCaptureDevice
     let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+    println!("got requested");
     let mut camera = Camera::new(index, requested).context("build new camera")?;
+    println!("got camera");
     Ok(())
 }
