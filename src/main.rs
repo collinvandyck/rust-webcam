0c0acbc965f868ff8f3d8bbf3829329479ff2bb8
Collin Van Dyck <collin@5xx.engineer>
2025-09-03 12:08:36 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use nokhwa::{
+    Camera,
+    pixel_format::RgbFormat,
+    utils::{CameraIndex, RequestedFormat, RequestedFormatType},
+};
 
-fn main() {
-    run().unwrap()
-}
-
-fn run() -> Result<()> {
+fn main() -> Result<()> {
+    let index = CameraIndex::Index(0);
+    let requested = RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
+    let mut camera = Camera::new(index, requested).context("build new camera")?;
     Ok(())
 }
